90e4434bfdc393dea42081a94a49b34b3092b74d
TGaelzer <tamigaelzer@gmail.com>
2022-01-16 16:41:01 -0300

    Tentar organizar script

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const imagem = document.querySelector('img');
+const imagem1 = document.querySelector('#img1');
+const imagem2 = document.querySelector('#img2');
+const imagem3 = document.querySelector('#img3');
 const botao = document.querySelector('#botao');
-const nomeDoPersonagem = document.querySelector ('#nome');
-const especieDoPersonagem = document.querySelector ('#especie');
-const statusVida = document.querySelector ('#status');
+const nomeDoPersonagem1 = document.querySelector ('#nome');
+const nomeDoPersonagem2 = document.querySelector ('#nome2');
+const nomeDoPersonagem3 = document.querySelector ('#nome3');
+const especieDoPersonagem1 = document.querySelector ('#especie');
+const especieDoPersonagem2 = document.querySelector ('#especie2');
+const especieDoPersonagem3 = document.querySelector ('#especie3');
+const statusVida1 = document.querySelector ('#status');
+const statusVida2 = document.querySelector ('#status2');
+const statusVida3 = document.querySelector ('#status3');
+
+var gerarTime = [pegarPersonagem1, pegarPersonagem2, pegarPersonagem3]
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
-pegarPersonagem = () => {  
+pegarPersonagem1 = () => {  
     let numeroAleatorio = gerarValorAleatorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
@@ -17,12 +27,46 @@ pegarPersonagem = () => {
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        statusVida.innerHTML = data.status;
+        imagem1.src = data.image;
+        imagem1.alt = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        statusVida1.innerHTML = data.status;
     });
 }
 
-botao.onclick = pegarPersonagem;
+pegarPersonagem2 = () => {  
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        statusVida2.innerHTML = data.status;
+    });
+}
+
+pegarPersonagem3 = () => {  
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;
+        imagem3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        statusVida3.innerHTML = data.status;
+    });
+}
+
+botao.onclick = gerarTime;
